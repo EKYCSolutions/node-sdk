2d2a19fbc21c6f7aca546a969fc6a6ea3dfe4afa
ShadowLegend <lyheng0@gmail.com>
2022-03-18 22:22:11 +0700

    adding test

--- packages/ml-vision/__tests__/ml-vision.test.js
+++ packages/ml-vision/__tests__/ml-vision.test.js
@@ -1,6 +1,23 @@
 
+import path from 'path';
+
+import { EkycClient } from '@ekycsolutions/client';
+
 import { MLVision } from '../dist/index';
 
-describe('@ekycsolutions/ml', () => {
-  it('needs tests', () => {});
+describe('@ekycsolutions/ml-vision', () => {
+  it('needs tests', async () => {
+    const ekycClient = new EkycClient({
+      serverAddress: 'https://server.localhost:4443',
+      auth: {
+        caCertificatePath: '/tmp/ca.cert.pem',
+        clientCertSavePath: '/tmp/client.cert.pem',
+        apiKeyPath: path.resolve('../../packages/auth/__tests__', 'test-key.json'),
+      },
+    });
+
+    const mlVision = new MLVision(ekycClient);
+
+    console.log(await ekycClient.apiResultPolling('abc123123888888'));
+  });
 });
