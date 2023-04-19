dd72d1d3c77dd706992db1c8d0191bfc8fa873ac
pisethchhom <pisethchhom@gmail.com>
2023-04-19 15:16:17 +0700

    add liveliness_config handlers

---
+++ packages/fastify-routes/src/handlers/liveness_config.ts
@@ -0,0 +1,40 @@
+import { Sqlite } from '../sqlite.js';
+import { livenessConfigResponseSchema } from '../responses/liveness_config.js';
+
+export const livenessUpdateSchema = {
+    response: {
+        200: livenessConfigResponseSchema,
+    },
+    body: {
+        type: 'object',
+        // required: ['enable'],
+        properties: {
+            enable: {
+                type: 'string',
+                enum: ['yes', 'no']
+            }
+        },
+    },
+};
+
+export async function livenessQueryHandler(opts, request, reply) {
+    const sqliteDb: Sqlite = (request as any).sqliteDb;
+    const result = await sqliteDb.queryRecord(`SELECT enable FROM ${sqliteDb.livenessTable} LIMIT 1`);
+
+    reply.send(result);
+};
+
+export async function livenessUpdateHandler(opts, request, reply) {
+    const sqliteDb: Sqlite = (request as any).sqliteDb;
+    const body = request.body as any;
+    
+    await sqliteDb.runQueryRecord(
+        `UPDATE ${sqliteDb.livenessTable} SET enable = ?`, 
+        [body.enable]
+    );
+   
+    const result = await sqliteDb.queryRecord(`SELECT enable FROM ${sqliteDb.livenessTable} LIMIT 1`);
+
+    reply.send(result);
+};
+
