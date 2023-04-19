abb54fc7b20b506f46926361de76d1976086057c
pisethchhom <pisethchhom@gmail.com>
2023-04-19 14:15:36 +0700

    add method livelinessDetection and param

--- packages/ml-vision/src/index.ts
+++ packages/ml-vision/src/index.ts
@@ -27,6 +27,15 @@ export interface IdDetectionParams extends CommonMLVisionParams {
   imageUrl: string;
 }
 
+export interface LivenessDetectionSequence {
+    video_url: string;
+    checks: string;
+}
+
+export interface LivenessDetectionParams extends CommonMLVisionParams {
+  sequences: LivenessDetectionSequence[];
+}
+
 export class MLVision {
   constructor(private readonly ekycClient: EkycClient) {}
 
@@ -82,5 +91,27 @@ export class MLVision {
     });
 
     return await this.ekycClient.makeRequest('v0/id-detection', requestOpts);
+  }
+
+  public async livenessDetection({ sequences }: Readonly<LivenessDetectionParams>): Promise<ApiResult> {
+    const formData = this.ekycClient.prepareFormData({
+      api: 'liveness-detection',
+      version: 'v0',
+    });
+
+    for (let index = 0; index < sequences.length; index++) {
+      const sequence = sequences[index];
+      
+      formData.append(`sequences[${index}][checks]`, sequence.checks);
+      formData.append(`sequences[${index}][video_url]`, sequence.video_url);
+    }
+
+    const requestOpts = new Options({
+      body: formData,
+      method: 'POST',
+      headers: formData.getHeaders(),
+    });
+
+    return await this.ekycClient.makeRequest('v0/liveness-detection', requestOpts);
   }
 }
