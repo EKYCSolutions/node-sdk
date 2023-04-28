1480f2b87eeb7ee064836974a0553cda7c08a2d7
pisethchhom <pisethchhom@gmail.com>
2023-04-28 22:49:50 +0700

    add manual kyc config handlers

---
+++ packages/fastify-routes/src/handlers/manual_kyc_config.ts
@@ -0,0 +1,40 @@
+import { Sqlite } from '../sqlite.js';
+import { manualKycConfigResponseSchema } from '../responses/manual_kyc_config.js';
+
+export const manualKycUpdateSchema = {
+    response: {
+        200: manualKycConfigResponseSchema,
+    },
+    body: {
+        type: 'object',
+        // required: ['enable'],
+        properties: {
+            enable: {
+                type: 'string',
+                enum: ['yes', 'no']
+            }
+        },
+    },
+};
+
+export async function manualKycQueryHandler(opts, request, reply) {
+    const sqliteDb: Sqlite = (request as any).sqliteDb;
+    const result = await sqliteDb.queryRecord(`SELECT enable FROM ${sqliteDb.manualKycTable} LIMIT 1`);
+
+    reply.send(result);
+};
+
+export async function manualKycUpdateHandler(opts, request, reply) {
+    const sqliteDb: Sqlite = (request as any).sqliteDb;
+    const body = request.body as any;
+    
+    await sqliteDb.runQueryRecord(
+        `UPDATE ${sqliteDb.manualKycTable} SET enable = ?`, 
+        [body.enable]
+    );
+   
+    const result = await sqliteDb.queryRecord(`SELECT enable FROM ${sqliteDb.manualKycTable} LIMIT 1`);
+
+    reply.send(result);
+};
+
