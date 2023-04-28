c8699baca25fe950c2b89a2fdaa2e2f237a25917
xshadowlegendx <lyheng0@gmail.com>
2023-04-28 15:39:14 +0700

    refactor some code

---
+++ packages/fastify-routes/src/api-metadata.ts
@@ -0,0 +1,24 @@
+
+
+export const apiMetadata = {
+  'ocr': [{
+    version: 0,
+    versionName: 'v0',
+  }],
+  'face-compare': [{
+    version: 0,
+    versionName: 'v0',
+  }],
+  'id-detection': [{
+    version: 0,
+    versionName: 'v0',
+  }],
+  'liveness-detection': [{
+    version: 0,
+    versionName: 'v0',
+  }],
+  'manual-kyc': [{
+    version: 0,
+    versionName: 'v0',
+  }]
+};
---
+++ packages/fastify-routes/src/types.ts
@@ -0,0 +1,37 @@
+
+import { ApiResult } from '@ekycsolutions/client';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export interface EkycRoutesOpts {
+  serverUrl: string;
+  isServeUploadFiles?: boolean;
+  fileStorageDriver: 's3' | 'local';
+  onMlApiResult?: (mlApiResult: ApiResult, metadata: OnMlApiMetadata) => any;
+  preMlRequest?: (fastifyContext: FastifyLifecycleHookContext, mlContext: FastifyLifecycleHookMlContext) => Promise<void>;
+  postMlRequestBeforeSend?: (fastifyContext: FastifyLifecycleHookContext, mlContext: FastifyLifecycleHookMlContext) =>
+    Promise<{ error: any; newPayload: any; }>;
+  s3?: {
+    host: string;
+    port: number;
+    scheme: string;
+    bucket: string;
+    region: string;
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+}
+
+export interface OnMlApiMetadata {
+  apiName: string;
+  apiVersion: string;
+}
+
+export interface FastifyLifecycleHookMlContext {
+  apiResult: ApiResult;
+  metadata: OnMlApiMetadata;
+}
+
+export interface FastifyLifecycleHookContext {
+  req: FastifyRequest;
+  reply: FastifyReply;
+}
