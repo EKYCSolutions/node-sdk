6fa72f3968213177e1101ec756e14b978eecbba3
xshadowlegendx <lyheng0@gmail.com>
2023-06-28 11:43:43 +0700

    refactor for reusability

--- packages/ml-vision/src/index.ts
+++ packages/ml-vision/src/index.ts
@@ -2,48 +2,17 @@
 
 import { Options } from 'got';
 
-import { ApiResult, EkycClient } from '@ekycsolutions/client';
+import { ApiResult } from '@ekycsolutions/client';
+import { EkycClient } from '@ekycsolutions/client/dist/node';
+import {
+  OcrParams,
+  ManualKycParams,
+  FaceCompareParams,
+  IdDetectionParams,
+  LivenessDetectionParams,
+} from './interface.js';
 
-export interface CommonMLVisionParams {
-  version?: 0;
-  extraArgs?: {};
-}
-
-export interface FaceCompareParams extends CommonMLVisionParams {
-  faceImage0Url: string;
-  faceImage1Url: string;
-}
-
-export enum OcrObjectType {
-  nationalId = 'NATIONAL_ID_0',
-}
-
-export interface OcrParams extends CommonMLVisionParams {
-  imageUrl: string;
-  isRaw?: 'yes' | 'no';
-  objectType: OcrObjectType;
-}
-
-export interface IdDetectionParams extends CommonMLVisionParams {
-  imageUrl: string;
-}
-
-export interface LivenessDetectionSequence {
-  video_url: string;
-  checks: string;
-}
-
-export interface LivenessDetectionParams extends CommonMLVisionParams {
-  sequences: LivenessDetectionSequence[];
-}
-
-export interface ManualKycParams extends CommonMLVisionParams {
-  sequences: LivenessDetectionSequence[];
-  faceImageUrl: string;
-  ocrImageUrl: string;
-  isRaw?: 'yes' | 'no';
-  objectType: OcrObjectType;
-}
+export * from './interface.js';
 
 export class MLVision {
   constructor(private readonly ekycClient: EkycClient) { }
