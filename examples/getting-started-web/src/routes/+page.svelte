5e3cf0ec758fd17030efe22abdee689e3eb9ed0a
xshadowlegendx <lyheng0@gmail.com>
2023-06-30 11:58:00 +0700

    update to use simple api key as example instead

--- examples/getting-started-web/src/routes/+page.svelte
+++ examples/getting-started-web/src/routes/+page.svelte
@@ -7,17 +7,21 @@
 		serverAddress: 'http://localhost:4000',
 		auth: {
 			apiKey: {
-				key_id: '0b3509bb-07e9-4858-956e-0cd07d090bab',
-				app_id: 'http://localhost:5173',
-				client_key: 'ngwDXADnSBxuWcIiRBSQRPDnAjUZR1DRg_5_HnUiIoFKLCTwBLlQK-WDLocro_Y64uRNYwNhcOj_uLfSuVNR2CLcspdh5khpFn_KfpLgvvY9miNU5vIx35MrU-yjYEC2RVkZ6Jp9AhLln2WTich151E0li8pbFwnyOlbsF0pI7idrFAVM3wiTa4SvopSi38ZvRd6PwakrGV5PSrM-QoWSF3xx6GRx0vVDNrjse1sKgXb5gNHghRQE94AxiZDqynx',
-				client_pkey: 'PnEVT1eyaFq6nXG2ZhT9JvAaserEsbl7HwUkpduk4fo=',
+				api_key: '486fb5f9-3a1f-4d4c-831b-800cb2633020',
 			},
 		},
 	});
 
+	console.log(ekycClient);
+
 	const mlVision = new MLVisionBrowser(ekycClient);
 
-	createPublishableKey().then(console.log);
+	ekycClient.getRequestOpts().then(console.log);
+
+	createPublishableKey().then(k => {
+		console.log(k.toSave());
+		console.log(k.toDownloadJson({ appId: 'http://localhost:5173', keyId: 'abc' }));
+	});
 	
 	let ocrImage: FileList;
 	let faceImage: FileList;
