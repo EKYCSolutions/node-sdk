a22cdb75431d0c534d634716a27f7ab153730ba2
xshadowlegendx <lyheng0@gmail.com>
2023-08-30 14:07:30 +0700

    add optinal ml req args

--- packages/fastify-routes/src/types.ts
+++ packages/fastify-routes/src/types.ts
@@ -24,6 +24,7 @@ export interface EkycRoutesOpts {
 export interface OnMlApiMetadata {
   apiName: string;
   apiVersion: string;
+  mlReqArgs?: any;
 }
 
 export interface FastifyLifecycleHookMlContext {
